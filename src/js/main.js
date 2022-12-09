878c5015a485dd33d40a8900f020a8477d4b5b4c
k3n <racelisjk@gmail.com>
2022-12-09 20:51:22 +0800

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Import our custom CSS
+import "../scss/styles.scss";
+
+// Import all of Bootstraps JS
+import * as bootstrap from "bootstrap";
+
+// icons
+import "bootstrap-icons/font/bootstrap-icons.css";
